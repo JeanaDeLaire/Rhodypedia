8fd74fd56c955fb993e283ddbe82ea2a2a839a8a
JeanaDeLaire <jeanadelaire@gmail.com>
2019-02-08 19:52:29 -0500

    remove code comments from handlebars helper

--- assets/scripts/templates/helpers/limit.js
+++ assets/scripts/templates/helpers/limit.js
@@ -2,8 +2,6 @@
 
 const store = require('../../store.js')
 
-// return true if current user created post
-// if a user is not signed in create store.user.id and set as string
 const limit = (user_id) => {
   if (store.user === undefined) {
     store.user = {
